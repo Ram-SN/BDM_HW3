fa4b304194205e83d748064778e535dc7a91f5c3
Ram Sowmya Narayanan <rangamannar98@gmail.com>
2020-04-03 18:56:27 -0400

    TESTING

--- BDM_HW.py
+++ BDM_HW.py
@@ -1,72 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
 from pyspark import SparkContext
 
 
 input_file = sys.argv[1]
 
-output_file = sys.argv[2]
+output_file = sys.argv[2] ####name of the folder
 
+file = spark.read.csv(input_file, header=True)
 
+file.createOrReplaceTempView('file')
 
-
-
-
-
-
-
-
-
-
-
-def filterBike(records):
-    for record in records:
-        fields = record.split(',')
-        if (fields[6]=='Greenwich Ave & 8 Ave' and 
-            fields[3].startswith('2015-02-01')):
-            yield (fields[3][:19], 1)
-
-
-def filterTaxi(pid, lines):
-    if pid==0:
-        next(lines)
-    import pyproj
-    proj = pyproj.Proj(init="epsg:2263", preserve_units=True)
-    station = proj(-74.00263761, 40.73901691)
-    squared_radius = 1320**2
-    for trip in lines:
-            fields = trip.split(',')
-            if 'NULL' in fields[4:6]: continue # ignore trips without locations
-            dropoff = proj(fields[5], fields[4])
-            squared_distance = (dropoff[0]-station[0])**2 + (dropoff[1]-station[1])**2
-            if (fields[1].startswith('2015-02-01') and
-                squared_distance <= squared_radius):
-                yield (fields[1][:19], 0)
-
-def connectTrips(_, records):
-    import datetime
-    lastTaxiTime = None
-    count = 0
-    for dt,mode in records:
-        t = datetime.datetime.strptime(dt, '%Y-%m-%d %H:%M:%S')
-        if mode==1:
-            if lastTaxiTime!=None:
-                diff = (t-lastTaxiTime).total_seconds()
-                if diff>=0 and diff<=600:
-                    count += 1
-        else:
-            lastTaxiTime = t
-    yield(count)
-
-
-
-if __name__=='__main__':
-    sc = SparkContext()
-    bike = sc.textFile('/tmp/bdm/citibike.csv')
-    taxi = sc.textFile('/tmp/bdm/yellow.csv.gz')
-    matchedBike = bike.mapPartitions(filterBike)
-    bikeStation = (-74.00263761, 40.73901691)
-    matchedTaxi = taxi.mapPartitionsWithIndex(filterTaxi)
-
-    allTrips = (matchedBike+matchedTaxi).sortByKey().cache()
-
-    counts = allTrips.mapPartitionsWithIndex(connectTrips).reduce(lambda x,y: x+y)
-    print(counts)
+file.head()
