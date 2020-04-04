b8d58c20f264b507fb63f5d454b04cdb6e0e5b71
Ram Sowmya Narayanan <rangamannar98@gmail.com>
2020-04-04 18:02:22 -0400

    TESTING

--- BDM_HW.py
+++ BDM_HW.py
@@ -7,9 +7,6 @@ import sys
 import pyspark
 from pyspark import SparkContext
 from pyspark.sql.session import SparkSession
-from pyspark.sql.functions import col
-
-
 
 input_file = sys.argv[1]
 
@@ -20,19 +17,12 @@ df = spark.read.csv(input_file, header=True)
 
 df.createOrReplaceTempView('df')
 
-df = df.select(col("Date received").alias("Date_received"))
-
-df.show()
+#df.show()
 
 #df.printSchema()
+
+df = df.withColumnRenamed("Date received", "Date_received")
 
 test = spark.sql('SELECT Product, Date_received FROM df')
 
 test.show()
-
-
-# sc = SparkContext()
-
-# file = spark.read.csv(input_file)
-
-# file.head()
