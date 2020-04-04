3fbbf428fbb46b3b20f8613067156217d2413fbb
Ram Sowmya Narayanan <rangamannar98@gmail.com>
2020-04-03 20:14:25 -0400

    TESTING

--- BDM_HW.py
+++ BDM_HW.py
@@ -9,7 +9,7 @@ import sys
 def sample_print(records):
     for record in records:
         fields = record.split(',')
-            yield (fields)
+        yield (fields)
 
 if __name__=='__main__':
 
