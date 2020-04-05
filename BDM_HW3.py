4112b39656dcff318668ad0a249d02f7892487a3
Ram Sowmya Narayanan <rangamannar98@gmail.com>
2020-04-05 15:42:58 -0400

    TESTING

--- BDM_HW3.py
+++ BDM_HW3.py
@@ -19,6 +19,6 @@ test = df.select('Date received', 'Product', 'Company')
 
 res1 = test.select(year('Date received').alias('year'), 'Product', 'Company')
 
-res2 = res1.select('year', 'Product', 'Company').groupBy('Company', 'year').agg(func.count('Company')).
+res2 = res1.select('year', 'Product', 'Company').groupBy('Company', 'year').agg(func.count('Company'))
 
 #test.show()
