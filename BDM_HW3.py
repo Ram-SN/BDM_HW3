a525b8826ce684da553c53d533be06ce52e79be2
Ram Sowmya Narayanan <rangamannar98@gmail.com>
2020-04-04 21:41:24 -0400

    TESTING

---
+++ BDM_HW3.py
@@ -0,0 +1,16 @@
+from pyspark import SparkContext
+import pyspark.sql.functions as func
+from ptspark.sql.session import SparkSession
+
+sc = SparkContext()
+spark = SparkSession(sc)
+
+input_file = sys.argv[1]
+
+df = spark.read.csv(input_file, header=True, escape ='"', inferSchema = True, multiline = True)
+
+df.createOrReplaceTempView('df')
+
+test = spark.sql('SELECT EXTRACT(YEAR FROM Date received), Product FROM df')
+
+test.show()
