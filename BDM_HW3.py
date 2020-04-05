74cd1b247c5dd99fa0c05461338b50e39802fa78
Ram Sowmya Narayanan <rangamannar98@gmail.com>
2020-04-05 17:31:42 -0400

    TESTING

--- BDM_HW3.py
+++ BDM_HW3.py
@@ -23,7 +23,7 @@ res2 = res1.groupBy('year', 'Product', 'Company').agg(func.count('Product').alia
 
 #res3 = res2.groupBy('year','Product')
 
-res4 = res2.groupBy('year','Product').agg(func.sum('Count_comp').alias('Count_sum'),agg(func.max('Count_comp').alias('Count_max'))
+res4 = res2.groupBy('year','Product').agg(func.sum(res2.Count_comp).alias('Count_sum'),agg(func.max(res2.Count_comp).alias('Count_max'))
 
 res4.show()
 
